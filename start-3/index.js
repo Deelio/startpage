f88197d2c15edbc23e88dc459c14ef4f1e5d4b20
Deelio <wimbc@protonmail.com>
2020-11-25 15:58:11 -0500

    I am so useful to society

--- start-3/index.js
+++ start-3/index.js
@@ -1,29 +1,44 @@
 var left, center, right;
+var ll, lc, lr;
 
 function init() {
 	left = document.getElementById("l");
 	center = document.getElementById("c");
 	right = document.getElementById("r");
+
+	ll = document.getElementById("ll");
+	lc = document.getElementById("lc");
+	lr = document.getElementById("lr");
 }
 
 function ret() {
 	left.style.width = "33.33333%";
 	center.style.width = "33.33333%";
 	right.style.width = "33.33333%";
+	ll.style.display = "none";
+	lc.style.display = "none";
+	lr.style.display = "none";
 }
 
 function enL() {
 	left.style.width = "68%";
 	center.style.width = "16%";
 	right.style.width = "16%";
+
+	ll.style.display = "grid";
 }
 function enC() {
 	left.style.width = "16%";
 	center.style.width = "68%";
 	right.style.width = "16%";
+
+	lc.style.display = "grid";
 }
 function enR() {
 	left.style.width = "16%";
 	center.style.width = "16%";
 	right.style.width = "68%";
+
+	lr.style.display = "grid";
+
 }
